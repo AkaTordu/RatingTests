54b66d3f26a9257736a5033c140e38c28a69a273
AkaTordu <maximilienlandes@gmail.com>
2023-03-14 14:21:26 +0100

    deploiement serv V3

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
 // Middleware pour parser les données JSON
 app.use(bodyParser.json());
+app.set('view engine', 'ejs')
 
-// Tableau de notes initial
+// Lecture des notes depuis le fichier
 let notes = [];
+try {
+  notes = JSON.parse(fs.readFileSync('notes.json'));
+} catch (error) {}
 
 // Middleware pour autoriser les requêtes cross-origin
 app.use((req, res, next) => {
@@ -17,18 +22,24 @@ app.use((req, res, next) => {
 });
 
 // Route pour récupérer la note moyenne
-app.get('/api/notes/moyenne', (req, res) => {
+app.get('/moyenne', (req, res) => {
   const somme = notes.reduce((acc, note) => acc + note.note, 0);
   const moyenne = somme / notes.length;
   res.json({ moyenne: moyenne });
 });
 
 // Route pour enregistrer une nouvelle note
-app.post('/api/notes', (req, res) => {
+app.post('/notes', (req, res) => {
   const note = req.body.note;
   notes.push({ note: note });
+  fs.writeFileSync('notes.json', JSON.stringify(notes));
   res.json({ note: note });
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
 
 // Démarrage du serveur
 app.listen(3000, () => {
